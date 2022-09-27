d48d2c364a37fdead58cc6d301a85c36435a42c9
Arnaldo Gabriel <gabrielgcs.soares@gmail.com>
2022-09-26 21:32:06 -0300

    Init site

---
+++ public/style/estilo.js
@@ -0,0 +1,8 @@
+function move(){
+    const colorPassMouse = window.document.getElementById('link');
+    colorPassMouse.style.background = '#c9d6df';
+}
+function retire(){
+    const colorExitMouse = window.document.getElementById('link');
+    colorExitMouse.style.background = 'white';
+}
