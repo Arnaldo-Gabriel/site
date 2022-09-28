b6057b8b364d1c642e464fd7e0620d185f9b6d3b
Arnaldo Gabriel <gabrielgcs.soares@gmail.com>
2022-09-28 13:24:08 -0300

    Complementações JS

--- public/style/estilo.js
+++ public/style/estilo.js
@@ -1,3 +1,5 @@
+
+/*funcion para definir cor do botão principal*/
 function move(){
     const colorPassMouse = window.document.getElementById('link');
     colorPassMouse.style.background = '#c9d6df';
@@ -5,4 +7,96 @@ function move(){
 function retire(){
     const colorExitMouse = window.document.getElementById('link');
     colorExitMouse.style.background = 'white';
+}
+
+
+/*Alterações cor de body*/
+function moveArroz(){
+    const alterarArroz = window.document.querySelector('#first-file')
+    alterarArroz.style.background = 'url(./_imagens/arroz.jpg)';
+}
+function backArroz(){
+    const voltaFundo = window.document.querySelector('#first-file')
+    voltaFundo.style.background = 'url(../_imagens/R.jpg)';
+}
+
+function moveAveia(){
+    const alterarAveia = window.document.querySelector('#first-file')
+    alterarAveia.style.background = 'url(./_imagens/aveia.jpg)';
+}
+function backAveia(){
+    const voltaAveia = window.document.querySelector('#first-file')
+    voltaAveia.style.background = 'url(../_imagens/R.jpg)';
+}
+
+function moveBd(){
+    const alterarBd = window.document.querySelector('#first-file')
+    alterarBd.style.background = 'url(./_imagens/batata-doce.jpg)';
+}
+function backBd(){
+    const voltaBd = window.document.querySelector('#first-file')
+    voltaBd.style.background = 'url(../_imagens/R.jpg)';
+}
+
+function moveBi(){
+    const alterarBi = window.document.querySelector('#first-file')
+    alterarBi.style.background = 'url(./_imagens/batata-inglesa.png)';
+}
+function backBi(){
+    const voltaBi = window.document.querySelector('#first-file')
+    voltaBi.style.background = 'url(../_imagens/R.jpg)';
+}
+
+function moveBeterraba(){
+    const alterarBeterraba = window.document.querySelector('#first-file')
+    alterarBeterraba.style.background = 'url(./_imagens/beterraba.jpg)';
+}
+function backBeterraba() {
+    const voltaBeterraba = window.document.querySelector('#first-file')
+    voltaBeterraba.style.background = 'url(../_imagens/R.jpg)';
+}
+
+function moveCastanha(){
+    const alterarCastanha = window.document.querySelector('#first-file')
+    alterarCastanha.style.background = 'url(./_imagens/castanhasjpg.jpg)';
+}
+function backCastanha(){
+    const voltaCastanha = window.document.querySelector('#first-file')
+    voltaCastanha.style.background = 'url(../_imagens/R.jpg)';
+}
+
+function moveFeijaoPreto(){
+    const alterarFeijaoPreto = window.document.querySelector('#first-file')
+    alterarFeijaoPreto.style.background = 'url(./_imagens/feijao-preto.jpg)';
+}
+function backFeijaoPreto(){
+    const voltaFeijaoPreto = window.document.querySelector('#first-file')
+    voltaFeijaoPreto.style.background = 'url(../_imagens/R.jpg)';
+}
+
+function moveFruta(){
+    const alterarFruta = window.document.querySelector('#first-file')
+    alterarFruta.style.background = 'url(./_imagens/frutas.jpg)';
+}
+function backFruta(){
+    const voltaFruta = window.document.querySelector('#first-file')
+    voltaFruta.style.background = 'url(../_imagens/R.jpg)';
+}
+
+function moveGrao(){
+    const alterarGrao = window.document.querySelector('#first-file')
+    alterarGrao.style.background = 'url(./_imagens/grao-de-bico.jpg)';
+}
+function backGrao(){
+    const backGrao = window.document.querySelector('#first-file')
+    backGrao.style.background = 'url(../_imagens/R.jpg)';
+}
+
+function movePao(){
+    const alterarPao = window.document.querySelector('#first-file')
+    alterarPao.style.background = 'url(./_imagens/pao-integral.jpg)';
+}
+function backPao(){
+    const voltaPao = window.document.querySelector('#first-file')
+    voltaPao.style.background = 'url(../_imagens/R.jpg)';
 }
